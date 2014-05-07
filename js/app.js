0bb2b23bdddd64b177424bad6cf11873d929c5be
Lin Dong <lindong@gatech.edu>
2014-05-07 17:18:29 -0400

    Add render picture from 'aww'

--- js/app.js
+++ js/app.js
@@ -7,9 +7,13 @@ App.Router.map(function() {
 });
 
 App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+    model: function(){
+        return App.RedditLink.findAll('aww');
+    }
+});
+
+App.IndexController = Ember.ObjectController.extend({
+    subredditHeader: 'aww'
 });
 
 App.RedditLink = Ember.Object.extend({
@@ -24,10 +28,11 @@ App.RedditLink = Ember.Object.extend({
 App.RedditLink.reopenClass({
     findAll: function(subreddit){
         var links =[];
-        $.getJSON('http://www.reddit.com/r/'+ +subreddit + '/json?jsonp=?').then(function(response){
+        $.getJSON('http://www.reddit.com/r/'+ subreddit + '/.json?jsonp=?').then(function(response){
             response.data.children.forEach(function(child){
                links.pushObject(App.RedditLink.create(child.data));
             });
         });
+        return links;
     }
 });
